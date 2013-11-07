ae3b1d11032cd19c2e336cc91c75f70aa39bcd28
Kiril Nikolov <kiril.nikolov@gmail.com>
2013-11-07 08:51:50 -0600

    Changes made by Kiril Nikolov (kiril.nikolov@gmail.com).

--- hackathon/scripts/app.js
+++ hackathon/scripts/app.js
@@ -1,25 +1,6 @@
-(function (global) {
-    var mobileSkin = "",
-        app = global.app = global.app || {},
-        os = kendo.support.mobileOS,
-        statusBarStyle = os.ios && os.flatVersion >= 700 ? "black-translucent" : "black";
+function tap(e){
+    var navTo = + e.sender.element[0].innerText.toLowerCase();
+    app.navigate(navTo);
+}
 
-    document.addEventListener('deviceready', function () {
-        navigator.splashscreen.hide();
-    }, false);
-
-    app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout", statusBarStyle: statusBarStyle });
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
-})(window);
+var app = new kendo.mobile.Application()
