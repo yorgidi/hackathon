05fcb362995f19f6d05dde2a02310284914493d7
Kiril Nikolov <kiril.nikolov@gmail.com>
2013-11-07 09:12:58 -0600

    nav added

--- hackathon/scripts/app.js
+++ hackathon/scripts/app.js
@@ -1,6 +1,18 @@
-function tap(e){
-    var navTo = + e.sender.element[0].innerText.toLowerCase();
-    app.navigate(navTo);
+function navPeople(){
+    app.navigate("#people");
 }
+function navCars(){
+    app.navigate("#cars");
+}
+function navMassages(){
+    app.navigate("#massages");
+}
+function navLeaves(){
+    app.navigate("#leaves");
+}
+function navMenu(){
+    app.navigate("#menu");
+}
+
 
 var app = new kendo.mobile.Application()
