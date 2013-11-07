3ea970c6c6339650761b8ffecad6f64e27523600
Kiril Nikolov <kiril.nikolov@gmail.com>
2013-11-07 10:06:15 -0600

    added latest kendo scripts

--- hackathon/scripts/app.js
+++ hackathon/scripts/app.js
@@ -1,18 +1,42 @@
-function navPeople(){
+function navPeople() {
     app.navigate("views/people.html");
 }
-function navCars(){
+function navCars() {
     app.navigate("views/cars.html");
 }
-function navMassages(){
+function navMassages() {
     app.navigate("views/massages.html");
 }
-function navLeaves(){
+function navLeaves() {
     app.navigate("views/leaves.html");
 }
-function navMenu(){
+function navMenu() {
     app.navigate("views/menu.html");
 }
 
+function mobileListViewFiltering() {
+    var dataSource = new kendo.data.DataSource({
+        transport: {
+            read: function(o){
+                o.success(peopleData);
+            }
+        },
+        sort: {
+            field: "name",
+            dir: "asc"
+        },
+        pageSize: 50
+    });
+
+    $("#filterable-listview").kendoMobileListView({
+        dataSource: dataSource,
+        template: $("#mobile-listview-filtering-template").text(),
+        filterable: {
+            field: "name",
+            operator: "startswith"
+        },
+        endlessScroll: true
+    });
+}
 
 var app = new kendo.mobile.Application()
