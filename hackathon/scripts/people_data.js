268225e6530a1a60928d07a69d67431f5e897876
Alexander Vakrilov <alexander.vakrilov@telerik.com>
2013-11-08 10:39:39 -0600

    Pictures

--- hackathon/scripts/people_data.js
+++ hackathon/scripts/people_data.js
@@ -1,81 +1,81 @@
 var peopleData = [
-{
-    name: "Kiril Nikolov",
-    email: "kiril.nikolov@telerik.com",
-    team:"Kendo UI",
-    phone: "+359887405602"
-},
-{
-    name: "Georgi Mateev",
-    email: "Georgi.Mateev@telerik.com",
-    team:"Sitefinity",
-    phone: "+359887546981"
-},
-{
-    name: "Viktor Bukurov",
-    email: "viktor.bukurov@telerik.com",
-    team:"Sitefinity",
-    phone: "+359887549181"
-},
-{
-    name: "Alexander Vakrilov",
-    email: "alexander.vakrilov@telerik.com",
-    team:"XAML",
-    phone: "+359895657439"
-},
-{
-    name: "Stanimir Karoserov",
-    email: "stanimir.karoserov@telerik.com",
-    team:"xPlatCore",
-    phone: "+359896895148"
-},
-{
-    name: "Petya Bogdanova",
-    email: "petya.bogdanova@telerik.com",
-    team:"Sales",
-    phone: "+359889147648"
-},
-{
-    name: "Nikola Irinchev",
-    email: "nikola.irinchev@telerik.com",
-    team:"Icenium",
-    phone: "+359896942848"    
-},
-{
-    name: "Yordan Dimitrov",
-    email: "yordan.dimitrov@telerik.com",
-    team:"Team Puls",
-    phone: "+359896895865"
-},
-{
-    name: "Svetozar Gerogiev",
-    email: "svetozar.georgiev@telerik.com",
-    team:"CEO",
-    phone: "+359896111777"
-},
-{
-    name: "Todd Anglin",
-    email: "todd.anglin@telerik.com",
-    team:"VP",
-    phone: "+1564753247"
-},
-{
-    name: "Mihail Valkov",
-    email: "mihail.valkov@telerik.com",
-    team:"Dev. Manager",
-    phone: "+359887584263"   
-},
-{
-    name: "Stephen Forte",
-    email: "stephen.forte@telerik.com",
-    team:"CSO",
-    phone: "+359896555888"
-},
-{
-    name: "Doug Laird",
-    email: "doug.laird@telerik.com",
-    team:"CMO",
-    phone: "+155584756"
-}
-
+    {
+        name: "Todd Anglin",
+        email: "todd.anglin@telerik.com",
+        team:"VP",
+        phone: "+1564753247",
+        imageUrl:"images/people/todd-anglin.jpg",
+    },{
+        name: "Doug Laird",
+        email: "doug.laird@telerik.com",
+        team:"CMO",
+        phone: "+155584756",
+        imageUrl:"images/people/doug-laird.jpg",
+    },{
+        name: "Petya Bogdanova",
+        email: "petya.bogdanova@telerik.com",
+        team:"Sales",
+        phone: "+359889147648",
+        imageUrl:"images/people/petya-bogdanova.jpg",    
+    },{
+        name: "Kiril Nikolov",
+        email: "kiril.nikolov@telerik.com",
+        team:"Kendo UI",
+        phone: "+359887405602",
+        imageUrl:"images/people/kiril-nikolov.jpg",        
+    },{
+        name: "Georgi Mateev",
+        email: "Georgi.Mateev@telerik.com",
+        team:"Sitefinity",
+        phone: "+359887546981",
+        imageUrl:"images/people/petya-bogdanova.jpg",    
+    },{
+        name: "Viktor Bukurov",
+        email: "viktor.bukurov@telerik.com",
+        team:"Sitefinity",
+        phone: "+359887549181",
+        imageUrl:"images/people/viktor-bukurov.jpg",    
+    },{
+        name: "Alexander Vakrilov",
+        email: "alexander.vakrilov@telerik.com",
+        team:"XAML",
+        phone: "+359895657439",
+        imageUrl:"images/people/alexander-vakrilov.jpg",    
+    },{
+        name: "Stanimir Karoserov",
+        email: "stanimir.karoserov@telerik.com",
+        team:"xPlatCore",
+        phone: "+359896895148",
+        imageUrl:"images/people/stanimir-karoserov.jpg",    
+    },{
+        name: "Nikola Irinchev",
+        email: "nikola.irinchev@telerik.com",
+        team:"Icenium",
+        phone: "+359896942848",
+        imageUrl:"images/people/nikola-irinchev.jpg",    
+    },{
+        name: "Yordan Dimitrov",
+        email: "yordan.dimitrov@telerik.com",
+        team:"Team Puls",
+        phone: "+359896895865",
+        imageUrl:"images/people/ydimitrov.jpg",    
+    },{
+        name: "Svetozar Gerogiev",
+        email: "svetozar.georgiev@telerik.com",
+        team:"CEO",
+        phone: "+359896111777",
+        imageUrl:"images/people/svetozar-georgiev.jpg",    
+    },{
+        name: "Mihail Valkov",
+        email: "mihail.valkov@telerik.com",
+        team:"Dev. Manager",
+        phone: "+359887584263",   
+        imageUrl:"images/people/mihail-valkov.jpg",    
+    },{
+        name: "Stephen Forte",
+        email: "stephen.forte@telerik.com",
+        team:"CSO",
+        phone: "+359896555888",
+        imageUrl:"images/people/stephen-forte.jpeg",    
+    }
 ]
